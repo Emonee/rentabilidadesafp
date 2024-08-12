a533cd43f82f5ae6960707c633eb5f30e9e9d5ee
Emiliano Acevedo <e.acevedo.felip@gmail.com>
2024-08-11 22:32:54 -0400

    impr: remove astro comp

---
+++ src/components/tables/BestAfpByYTDTable.tsx
@@ -0,0 +1,34 @@
+import { getColor } from '@/lib/utilities/nums'
+import { For } from 'solid-js'
+
+type Props = {
+  tableRows: Array<{ name: string; value: number }>
+}
+
+export default function BestAfpByYTDTable(props: Props) {
+  return (
+    <table style="max-width: 500px;">
+      <tbody>
+        <For each={props.tableRows}>
+          {({ name, value }) => {
+            const color = getColor(10, -10, value)
+            const colorStyle = `4px solid ${color}`
+            return (
+              <tr>
+                <td style={{ 'border-left': colorStyle }}>{name}</td>
+                <td
+                  style={{
+                    'border-left': colorStyle,
+                    'border-right': colorStyle
+                  }}
+                >
+                  {value}
+                </td>
+              </tr>
+            )
+          }}
+        </For>
+      </tbody>
+    </table>
+  )
+}
